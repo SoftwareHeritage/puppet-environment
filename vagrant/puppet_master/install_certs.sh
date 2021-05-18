d0cb8faec4b6659c3f1476da8b44efbb8ad2b243
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2021-05-18 18:23:29 +0200

    vagrant/puppet_master/install_certs.sh: Install postgres
    Temporary hack to install postgresql prior to puppetdb.
    
    Related to T3325

--- vagrant/puppet_master/install_certs.sh
+++ vagrant/puppet_master/install_certs.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -eu
 
 CERT_DIR="/tmp/puppet_master"
 PUPPET_CERT_DIR=/var/lib/puppet/ssl
 
 if [ ! -d ${PUPPET_CERT_DIR} ]; then
+  apt-get update
+  apt-get install -y postgresql
   mkdir -p ${PUPPET_CERT_DIR}/
   cp -r $CERT_DIR/* ${PUPPET_CERT_DIR}
   chown -R puppet:puppet ${PUPPET_CERT_DIR}
