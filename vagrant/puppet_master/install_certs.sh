8a9a5b08160fa563467ebd050479227e3e15cf5a
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2021-05-18 17:20:32 +0200

    Vagrantfile/pergamon: Install rsync in the provisioning step
    This fails otherwise [1]
    
    Either that or redo the debian image so it's installed there.
    
    [1]
    ```
    ==> pergamon: Running provisioner: shell...
        pergamon: Running: /tmp/vagrant-shell20210518-14488-1xk5s1k.sh
        pergamon: sudo
        pergamon: :
        pergamon: rsync: command not found
    ```
    
    Related to T3325

--- vagrant/puppet_master/install_certs.sh
+++ vagrant/puppet_master/install_certs.sh
@@ -3,8 +3,12 @@
 CERT_DIR="/tmp/puppet_master"
 PUPPET_CERT_DIR=/var/lib/puppet/ssl
 
-sudo mkdir -p ${PUPPET_CERT_DIR}/ 
+apt-get update
 
-sudo rsync -avP $CERT_DIR/* ${PUPPET_CERT_DIR}
+apt-get install -y rsync
 
-sudo chown -R puppet:puppet ${PUPPET_CERT_DIR}
+mkdir -p ${PUPPET_CERT_DIR}/
+
+rsync -avP $CERT_DIR/* ${PUPPET_CERT_DIR}
+
+chown -R puppet:puppet ${PUPPET_CERT_DIR}
