a00824d0f4f5bb4bbc743d397f6416d45e5fdefb
Vincent SELLIER <vincent.sellier@softwareheritage.org>
2022-01-21 16:05:27 +0100

    vagrant: update the facts at each run
    Related to T3873

--- vagrant/puppet_agent/install_facts.sh
+++ vagrant/puppet_agent/install_facts.sh
@@ -7,8 +7,6 @@ SUBNET=$2
 
 FACTS_D=/etc/facter/facts.d
 
-if [ ! -d $FACTS_D ] ; then
-    mkdir -p $FACTS_D
-    echo deployment=${DEPLOYMENT} > $FACTS_D/deployment.txt
-    echo subnet=${SUBNET} > $FACTS_D/subnet.txt
-fi
+mkdir -p $FACTS_D
+echo deployment=${DEPLOYMENT} > $FACTS_D/deployment.txt
+echo subnet=${SUBNET} > $FACTS_D/subnet.txt
